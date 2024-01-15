b68305755788ce5da25678eb7aa42d8336ce0ada
praiseike <praiseike123@gmail.com>
2024-01-15 14:54:35 +0100

    new commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Fade from "react-reveal";
 
 class Footer extends Component {
   render() {
@@ -17,6 +18,12 @@ class Footer extends Component {
     return (
       <footer>
         <div className="row">
+          <Fade bottom>
+            <div className="twelve columns">
+              <ul className="social-links">{networks}</ul>
+            </div>
+          </Fade>
+
           <div id="go-top">
             <a className="smoothscroll" title="Back to Top" href="#home">
               <i className="icon-up-open"></i>
